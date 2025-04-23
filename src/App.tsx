b34be1597dc1695988054e7021010007841d36ff
shinju4n <ju4nshin@gmail.com>
2025-04-23 16:13:34 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="w-full flex-center">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
